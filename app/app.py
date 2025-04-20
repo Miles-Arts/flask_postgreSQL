202ae5c5830f800c7f00bb9ddceab11bbfe60ad5
Milton Figueredo <gratno@gmail.com>
2025-04-19 21:59:57 -0500

    add query_string

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app=Flask(__name__)
 
@@ -25,7 +25,15 @@ def contacto(nombre,edad):
       return render_template('contacto.html',data=data)
 
 
+def query_string():
+      print(request)
+      print(request.args)
+      print(request.args.get('param1'))
+      print(request.args.get('param2'))
+      return "<h2>Ok ✅</h2>"
+
 if __name__ == '__main__':
+      app.add_url_rule('/query_string', view_func=query_string)
       app.run(debug=True,port=5050)
       
       
